af4bf6a63d2901bc6081f091892830397ae9cff9
jaythaceo <jaythaceo@gmail.com>
2015-05-02 19:46:27 -0400

    [chore] First function complete

--- src/google-python-exercises/basic/mimic.py
+++ src/google-python-exercises/basic/mimic.py
@@ -48,12 +48,29 @@ import sys
 def mimic_dict(filename):
   """Returns mimic dict mapping each word to list of words which follow it."""
   # +++your code here+++
-  return
+  mimic_dict = {}
+  f = open(filename, 'r')
+  text = f.read()
+  f.close()
+  words = text.split()
+  prev = ''
+  for word in words:
+      if not prev in mimic_dict:
+          mimic_dict[prev] = [word]
+      else:
+          mimic_dict[prev].append(word)
+  return mimic_dict
 
 
 def print_mimic(mimic_dict, word):
   """Given mimic dict and start word, prints 200 random words."""
   # +++your code here+++
+  for unused_i in range(200):
+      print word,
+      nexts = mimic_dict.get(word)
+      if not nexts:
+          nexts = mimic_dict['']
+      word = random.choice(nexts)
   return
 
 
