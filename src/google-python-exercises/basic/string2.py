52a42a5e0ae5340c0a811d30b37a149d1d57fb31
jaythaceo <jaythaceo@gmail.com>
2015-05-09 20:39:33 -0400

    [chore] Strings2 complete

--- src/google-python-exercises/basic/string2.py
+++ src/google-python-exercises/basic/string2.py
@@ -52,8 +52,10 @@ def front_back(a, b):
   a_middle = len(a) / 2
   b_middle = len(b) / 2
   if len(a) % 2 == 1:
-    pass
-  return
+    a_middle = a_middle + 1
+  if len(b) % 2 == 1:
+    b_middle = b_middle + 1
+  return a[:a_middle] + b[:b_middle] + a[a_middle:] + b[b_middle:]
 
 
 # Simple provided test() function used in main() to print
